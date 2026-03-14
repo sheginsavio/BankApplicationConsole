83f4375f70f88a7ebad23ff05abe655bc842b184
S S <2483421@cognizant.com>
2026-03-14 16:55:28 +0530

    Update on Program.cs

--- BankApplicationConsoleBasic/BankApplicationConsoleBasic/Program.cs
+++ BankApplicationConsoleBasic/BankApplicationConsoleBasic/Program.cs
@@ -10,7 +10,7 @@
                 Console.WriteLine("\n_____Menu_____");
                 Console.WriteLine("1.Create Account\n2.Show Account Information\n" +
                     "3.Deposit from account\n4.Withdraw from account\n" +
-                    "5.Show all account with id\n6.Clear screen\n7.Exit");
+                    "5.Show all account\n6.Clear screen\n7.Exit");
                 Console.WriteLine("Enter your choice:");
                 char input = Console.ReadKey().KeyChar;
                 Console.WriteLine();
@@ -21,17 +21,45 @@
                 }
                 int choice = (int)Char.GetNumericValue(input);
 
+                int index = 0;
+                string[,] arr = new string[10,2];
+
+
                 switch (choice){
                     case 1:
+                        index++;
 
+                        Console.WriteLine("Enter the Account Holder name:");
+                        arr[index, 0] = Console.ReadLine();
+                        Console.WriteLine("Enter the intial amount");
+                        arr[index, 1] = Console.ReadLine();
+
+                        Console.WriteLine("Account added successfully!!");
                         break;
                     case 2:
+                        Console.WriteLine("Enter the Account Holder name:");
+                        string name = Console.ReadLine();
+
+                        for (int i = 0; i < index; i++)
+                        {
+                            if (string.Equals(name,arr[i, 0], StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.WriteLine($"Account Holder name: {arr[i,0]}");
+                                Console.WriteLine($"Balance: {arr[i,1]}");
+                                break;
+                            }
+                        }
                         break;
                     case 3:
                         break;
                     case 4:
                         break;
                     case 5:
+                        for (int i = 0; i < index; i++)
+                        {
+                            Console.WriteLine($"Account Holder name: {arr[i, 0]}");
+                            Console.WriteLine($"Balance: {arr[i, 1]}");
+                        }
                         break;
                     case 6:
                         Console.Clear();
